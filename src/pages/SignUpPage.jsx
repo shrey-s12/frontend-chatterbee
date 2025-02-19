70d5c6470f304c7690252bc7fa97669fd3c4214e
Shrey Singhal <shreynbd12@gmail.com>
2025-02-19 23:24:07 +0530

    Design signUp page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,8 +1,140 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useAuthStore from '../store/useAuthStore';
+import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import AuthImagePattern from '../components/AuthImagePattern';
 
 const SignUpPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+  });
+
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => { };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>SignUpPage</div>
+    <div className='min-h-screen grid lg:grid-cols-2'>
+      {/* left side */}
+      <div className='flex flex-col justify-center items-center p-6 sm:p12'>
+
+        <div className='w-full max-w-md space-y-8'>
+          {/* LOGO */}
+          <div className='text-center mb-8'>
+            <div className='flex flex-col items-center gap-2 group'>
+              <div className='size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors'>
+                <MessageSquare className='size-6 text-primary' />
+              </div>
+              <h1 className='text-2xl font-bold mt-2'>Sign Up</h1>
+              <p className='text-base-content/60'>Get started with your free account</p>
+            </div>
+          </div>
+
+          {/* FORM */}
+          <form onSubmit={handleSubmit} className='space-y-6'>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text font-medium'>Full Name</span>
+              </label>
+              <div className='relative'>
+                <div className='absolute inset-y-0 lext-0 flex items-center pl-3 pointer-events-none'>
+                  <User className='size-5 text-base-content/40' />
+                </div>
+                <input
+                  type='text'
+                  placeholder='Full Name'
+                  className={`input input-bordered w-full pl-10`}
+                  value={formData.fullName}
+                  onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
+                />
+              </div>
+            </div>
+
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text font-medium'>Email</span>
+              </label>
+              <div className='relative'>
+                <div className='absolute inset-y-0 lext-0 flex items-center pl-3 pointer-events-none'>
+                  <Mail className='size-5 text-base-content/40' />
+                </div>
+                <input
+                  type='email'
+                  placeholder='Email'
+                  className={`input input-bordered w-full pl-10`}
+                  value={formData.email}
+                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                />
+              </div>
+            </div>
+
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text font-medium'>Password</span>
+              </label>
+              <div className='relative'>
+                <div className='absolute inset-y-0 lext-0 flex items-center pl-3 pointer-events-none'>
+                  <Lock className='size-5 text-base-content/40' />
+                </div>
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='Password'
+                  className={`input input-bordered w-full pl-10`}
+                  value={formData.password}
+                  onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                />
+                <button
+                  type='button'
+                  className='absolute inset-y-0 right-0 flex items-center pr-3'
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? (
+                    <EyeOff className='size-5 text-base-content/40' onClick={() => setShowPassword(false)} />
+                  ) : (
+                    <Eye className='size-5 text-base-content/40' onClick={() => setShowPassword(true)} />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <button type='submit' className='btn btn-primary w-full' disabled={isSigningUp}>
+              {isSigningUp ? (
+                <>
+                  <Loader2 className='size-5 animate-spin' />
+                  Loading...
+                </>
+              ) : (
+                "Create Account"
+              )}
+            </button>
+          </form>
+
+          <div className='text-center'>
+            <p className='text-base-content/60'>
+              Already have an account?{" "}
+              <Link to='/login' className='link text-primary'>
+                Sign In
+              </Link>
+            </p>
+          </div>
+
+        </div>
+      </div>
+
+      {/* right side */}
+
+      <AuthImagePattern
+        title='Welcome to ChatterBee'
+        subtitle="Connect with friends, share moments, and stay in touch with your loved ones."
+      />
+    </div>
   )
 }
 
