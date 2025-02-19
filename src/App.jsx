5984d327933072933c146fc0948ba2306b7de06c
Shrey Singhal <shreynbd12@gmail.com>
2025-02-19 20:27:15 +0530

    Test the DaisyUi working

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-red-500">Chatterbee</h1>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
     </>
   )
 }
