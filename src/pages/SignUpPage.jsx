fdfbde8aadcd85aa732ed9e47478851ac66fc181
Shrey Singhal <shreynbd12@gmail.com>
2025-02-19 23:57:26 +0530

    Add validation and toast in signup page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -3,6 +3,7 @@ import useAuthStore from '../store/useAuthStore';
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import AuthImagePattern from '../components/AuthImagePattern';
+import toast from 'react-hot-toast';
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -14,10 +15,21 @@ const SignUpPage = () => {
 
   const { signup, isSigningUp } = useAuthStore();
 
-  const validateForm = () => { };
+  const validateForm = () => {
+    if (!formData.fullName.trim()) return toast.error('Full Name is required');
+    if (!formData.email.trim()) return toast.error('Email is required');
+    if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error('Invalid email format');
+    if (!formData.password.trim()) return toast.error('Password is required');
+    if (formData.password.length < 6) return toast.error('Password must be at least 6 characters long');
+
+    return true;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const success = validateForm();
+
+    if (success === true) signup(formData);
   };
 
   return (
