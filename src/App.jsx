871cffb30e1f7f90ccb33960a6ef0df826ac1a9a
Shrey Singhal <shreynbd12@gmail.com>
2025-02-19 21:08:12 +0530

    Import all the pages in app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import Navbar from "./components/Navbar"
 import { Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
 
 
 function App() {
